aa251552a8f16f77e2512018b41818fb04b38483
Stefan Henning <hennings.brk@gmail.com>
2026-02-26 11:28:53 +0100

    add s3 bucket for avatrs incl. folder and png upload

---
+++ infrastructure/s3.tf
@@ -0,0 +1,27 @@
+# random hex value for bucket names
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+# s3 bucket
+resource "aws_s3_bucket" "avatars" {
+  bucket = "grocerymate-avatars"
+
+  tags = {
+    Name = "grocerymate-avatars"
+    Environment = "Dev"
+  }
+}
+
+#s3 create avatar folder
+resource "aws_s3_object" "folder" {
+  bucket = aws_s3_bucket.avatars.id
+  key    = "avatars/"
+}
+
+resource "aws_s3_object" "default_avatar" {
+  bucket = aws_s3_bucket.avatars.id
+  key    = "avatars/user_default.png"
+  source = "${path.module}/../backend/avatar/user_default.png"
+  content_type = "image/png"
+}
